4a1d747d4ed67fa35667ca4ba14eda431305b83c
chloechengr <chengsn@bc.edu>
2019-11-04 10:05:42 -0500

    9.2 Completed

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Chloe Cheng on 11/4/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
